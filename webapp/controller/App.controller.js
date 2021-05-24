a79d37cddc02538d1dc49a4f0f56b497cfeb5a38
Akash <Ak777545@gmail.com>
2021-05-24 12:57:09 +0530

    Data Model & Internationalization aka translation

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,7 +7,14 @@ sap.ui.define(
         "use strict";
         Controller.extend("opensap.myapp.controller.App", {
             onShowHello: function () {
-                MessageToast.show("Hello OpenSAP!");
+                // read msg from i18n model
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+                // show message
+                MessageToast.show(sMsg);
+
             }
         });
     });
